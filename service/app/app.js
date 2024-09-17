2715507b1de7949f0cb12229d58d0f9db40715d8
Aaditya Kasbekar <kasbekar.a@northeastern.edu>
2024-09-17 08:35:18 -0400

    Building Backend completed.

--- service/app/app.js
+++ service/app/app.js
@@ -3,16 +3,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import initRoutes from './routes/index.js';
 
-
 const init = (app) => {
-    app.use(cors());
-    app.use(express.json());
-    app.use(express.urlencoded())
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded())
 
-    app.use('/uploads', express.static('uploads'));
+  app.use('/uploads', express.static('uploads'));
 
-    mongoose.connect(process.env.MONGO_CONNECTION);
-    initRoutes(app);
+  mongoose.connect(process.env.MONGO_CONNECTION);
+  initRoutes(app);
 }
 
 export default init;
