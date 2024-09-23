bcfbc8556a1e2ff9423faffc551367265655996c
Aaditya Kasbekar <kasbekar.a@northeastern.edu>
2024-09-22 21:56:43 -0400

    Final Touch to deployement. Implemented different function for db connection.

--- service/app/app.js
+++ service/app/app.js
@@ -3,6 +3,18 @@ import express from 'express';
 import mongoose from 'mongoose';
 import initRoutes from './routes/routeIndex.js';
 
+const connectToDatabase = async (mongoURI) => {
+  try {
+    await mongoose.connect(mongoURI, {
+      serverSelectionTimeoutMS: 10000, // 10 seconds timeout for server selection
+    });
+    console.log('Successfully connected to MongoDB using Mongoose!');
+  } catch (err) {
+    console.error('Failed to connect to MongoDB:', err.message);
+    process.exit(1)
+  }
+};
+
 const init = (app) => {
   // Middleware setup
   app.use(cors());
@@ -13,20 +25,11 @@ const init = (app) => {
   // MongoDB URI from environment variables
   const mongoURI = process.env.MONGO_CONNECTION;
 
-  // Mongoose connection to MongoDB cluster
-  mongoose.connect(mongoURI, {
-    useNewUrlParser: true,           // Use the new MongoDB connection string parser
-    useUnifiedTopology: true,        // Opt into using the new connection management engine
-    serverSelectionTimeoutMS: 10000, // 10 seconds timeout for server selection
-  })
-    .then(() => console.log('Successfully connected to MongoDB using Mongoose!'))
-    .catch(err => {
-      console.error('Failed to connect to MongoDB:', err.message);
-      process.exit(1); // Exit if connection fails
-    });
+  // Connect to MongoDB
+  connectToDatabase(mongoURI);
 
   // Initialize application routes
   initRoutes(app);
-}
+};
 
 export default init;
